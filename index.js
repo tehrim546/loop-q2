c7b4a638413cc7abd9c4f234811635b2c323a62d
= <=>
2024-01-30 09:35:58 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+//Write a while loop that counts down from 10 to 1. Initialize a counter and decrement it in each iteration.
+let counter = 10;
+while (counter >= 1) {
+    console.log(counter);
+    counter--;
+}
---
+++ index.ts
@@ -0,0 +1,6 @@
+//Write a while loop that counts down from 10 to 1. Initialize a counter and decrement it in each iteration.
+let counter:number=10;
+while (counter >= 1){
+    console.log(counter);
+    counter--;
+}
